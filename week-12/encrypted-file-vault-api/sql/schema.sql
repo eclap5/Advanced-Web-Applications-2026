604d19fb881e80be2c69556cb56cddbd51aedb90
Aleksi Haapalainen <aleksi.haapalainen@gmail.com>
2026-03-23 00:15:04 +0200

    added dockerfile and missing field from sql schema file

--- week-12/encrypted-file-vault-api/sql/schema.sql
+++ week-12/encrypted-file-vault-api/sql/schema.sql
@@ -2,7 +2,8 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    encryption_key_fingerprint TEXT NULL
 );
 
 CREATE TABLE files (
